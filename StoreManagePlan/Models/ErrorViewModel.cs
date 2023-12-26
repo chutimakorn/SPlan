f011269ae26c30337efcb50f6040c129fe80db53
DESKTOP-TDTUP6V\tae_b <potsawat.chutimakorn@mirumagency.com>
2023-12-26 20:29:49 +0700

    new project

---
+++ StoreManagePlan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreManagePlan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
