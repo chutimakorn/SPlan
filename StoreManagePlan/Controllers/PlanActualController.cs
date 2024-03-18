540941d4f46e9d15a35476a50e5a84488a414ec5
Dol <doltree@gmail.com>
2024-03-19 02:57:09 +0700

    add Plan actual

---
+++ StoreManagePlan/Controllers/PlanActualController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManagePlan.Controllers
+{
+    public class PlanActualController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
