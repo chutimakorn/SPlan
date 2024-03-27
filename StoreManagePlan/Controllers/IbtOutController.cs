45aa84a3a690c0676d0379f29a42b11a406bd2ef
Dol <doltree@gmail.com>
2024-03-28 00:43:03 +0700

    ibt out

---
+++ StoreManagePlan/Controllers/IbtOutController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using OfficeOpenXml;
+using StoreManagePlan.Data;
+using StoreManagePlan.Models;
+using StoreManagePlan.Repository;
+using NuGet.Packaging.Core;
+using System.Text.Json;
+using Elfie.Serialization;
+using System.Globalization;
+using System.Xml.Linq;
+using System.Collections;
+
+namespace StoreManagePlan.Controllers
+{
+    public class IbtOutController : Controller
+    {
+        private readonly StoreManagePlanContext _context;
+        public static string _menu = "ibto";
+        private readonly IConfiguration _configuration;
+        private readonly IUtility _utility;
+        public IbtOutController(StoreManagePlanContext context, IConfiguration configuration, IUtility utility)
+        {
+            _context = context;
+            _configuration = configuration;
+            _utility = utility;
+        }
+        public IActionResult Index(int Store, int Spoke, int Day, int Week)
+        {
+
+            ViewBag.store = Store;
+            ViewBag.spoke = Spoke;
+            ViewBag.week = Week;
+            ViewBag.day = Day;
+            var newSpoke = Spoke;
+            var newStore = Store;
+
+            var weekList = _context.Week.Where(w => w.status == 2).ToList();
+            var storeList = _context.Store.Include(m => m.store_type).Where(s => s.store_type.store_type_name == "Hub").ToList();
+            var spokeList = _context.StoreRelation.Include(h => h.StoreHub).Include(s => s.StoreSpoke).Where(h => h.StoreHub.id == Store).Select(s => s.StoreSpoke).ToList();
+
+            var planactual = _context.PlanActually
+                .Where(s => s.store_id == Spoke && s.week_no == Week && s.day_of_week == Day)
+                .Include(i => i.item)
+                .AsEnumerable()
+                //.GroupBy(s => new { s.item, s.sku_id, s.week_no, s.day_of_week })
+                //.Select(group => new PlanActually {
+                //    sku_id = group.Key.sku_id,
+                //    week_no = group.Key.week_no,
+                //    day_of_week = group.Key.day_of_week,
+                //    plan_actually = group.Sum(pd => pd.plan_actually),
+                //    plan_value = group.Sum(pd => pd.plan_value),
+                //    item = group.Key.item,
+                //    approve = group.OrderBy(p => p.some_order_field).Select(p => p.approve).FirstOrDefault()
+                //})
+                .ToList();
+
+            ViewBag.storeList = storeList;
+            ViewBag.spokeList = spokeList;
+            ViewBag.weeklist = weekList;
+            ViewBag.role = Convert.ToInt32(HttpContext.Request.Cookies["Role"]);
+            ViewBag.menu = "pof";
+            return View(planactual);
+        }
+        public IActionResult Edit(int Store, int Spoke, int Day, int Week, int SkuId)
+        {
+            ViewBag.store = Store;
+            ViewBag.spoke = Spoke;
+            ViewBag.week = Week;
+            ViewBag.day = Day;
+            ViewBag.sku = SkuId;
+
+            ViewBag.reasonhight = _context.Reason.Where(m => m.menu == "ibt" && m.type == 1).ToList();
+            ViewBag.resonlow = _context.Reason.Where(m => m.menu == "ibt" && m.type == 0).ToList();
+            ViewBag.reson = _context.Reason.Where(m => m.menu == "ibt").ToList();
+
+            var spokeList = _context.StoreRelation.Include(h => h.StoreHub).Include(s => s.StoreSpoke).Where(h => h.StoreHub.id == Store).Select(s => s.StoreSpoke.id).ToList();
+
+            var planactual = _context.PlanActually
+                .Where(s => spokeList.Contains(s.store_id) && s.week_no == Week && s.day_of_week == Day && s.sku_id == SkuId)
+                .Include(i => i.item)
+                .Include(i => i.store)
+                .AsEnumerable()
+                .ToList();
+            ViewBag.role = Convert.ToInt32(HttpContext.Request.Cookies["Role"]);
+            ViewBag.menu = "pof";
+            return View(planactual);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SubmitAll(string planActuals)
+        {
+            try {
+                var jsonData = JsonConvert.DeserializeObject<List<PlanActually>>(planActuals);
+
+                foreach (var n in jsonData)
+                {
+                    var temp = await _context.PlanActually
+                        .Where(s => s.sku_id == n.sku_id && s.week_no == n.week_no && s.store_id == n.store_id
+                        && s.day_of_week == n.day_of_week).FirstOrDefaultAsync();
+                    temp.plan_actually = n.plan_actually;
+                    temp.reason_id = n.reason_id;
+                }
+
+                await _context.SaveChangesAsync();
+
+            }
+            catch (Exception ex) {
+                return Json(new { success = false, massage = ex.Message });
+            }
+
+            return Json(new { success = true, massage = "บันทีกสำเร็จ" });
+        }
+
+        public async Task<IActionResult> Approve(int Spoke, int Week, int Day)
+        {
+            try
+            {
+                var temp = await _context.PlanActually
+                        .Where(s => s.store_id == Spoke && s.week_no == Week && s.day_of_week == Day).ToListAsync();
+                foreach (var item in temp)
+                {
+                    item.approve = 1;
+                }
+                await _context.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, massage = ex.Message });
+            }
+
+            return Json(new { success = true, massage = "บันทีกสำเร็จ" });
+        }
+    }
+}
---
+++ StoreManagePlan/wwwroot/js/app/IbtOut.js
@@ -0,0 +1,215 @@
+﻿$(function () {
+
+    //var tableData = new DataTable('#table-data', {
+    //    "lengthMenu": [
+    //        [10, 25, 50, -1],
+    //        [10, 25, 50, 'All']
+    //    ]
+    //});
+
+
+    console.log("ready!");
+
+    $('.select-action').on('change', function () {
+        console.log('Selected value');
+        document.getElementById("form1").submit();
+    });
+
+    function submitForm() {
+        console.log("action!");
+        document.getElementById("form1").submit();
+    };
+
+    $('#printButton').click(function () {
+        window.print();
+    });
+
+    $('#submitButton').click(function () {
+        event.preventDefault();
+        var week = $("#html5-date-input-week").val();
+        var store = $("#html5-date-input-store").val();
+        var type = $("#html5-date-input-type").val();
+
+        var formData = new FormData();
+        formData.append('selectedStore', store);
+        formData.append('selectedWeek', week);
+        formData.append('type', type);
+        $.ajax({
+            url: '/PlanOfWeek/Approve', // Update the URL based on your actual URL structure
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+
+                if (data.success) {
+                    $('#import-success').toast('show');
+                    
+                }
+                else {
+                    $("#import-not-success .toast-body").text("อนุมัติไม่สำเร็จ ข้อความ: "+data.message);
+                    $('#import-not-success').toast('show');
+                }
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    $('#btn-edit').click(function () {
+        var store = $(this).data('store');
+        var sku = $(this).data('sku');
+        var day = $(this).data('day');
+        var week = $(this).data('week');
+        var spoke = $(this).data('spoke');
+
+        // Redirect the user to the desired URL
+        var url = '/IbtOut/Edit?Store=' + store + '&SkuId=' + sku + '&Day=' + day + '&Spoke=' + spoke + '&Week=' + week;
+        window.location.href = url;
+    });
+
+    $('#approve-all').click(function () {
+        event.preventDefault();
+        var store = $("#spokeSelect").val();
+        var week = $("#weekSelect").val();
+        var day = $("#daySelect").val();
+
+        var formData = new FormData();
+        formData.append('Spoke', store);
+        formData.append('Week', week);
+        formData.append('Day', day);
+        $.ajax({
+            url: '/IbtOut/Approve', // 
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+
+                if (data.success) {
+
+                    $('#status-app').text("ยืนยันแล้ว");
+                    $('#approve-all').prop('disabled', true);
+                    $('#import-success').toast('show');
+                }
+                else {
+                    $("#import-not-success .toast-body").text("อนุมัติไม่สำเร็จ ข้อความ: " + data.message);
+                    $('#import-not-success').toast('show');
+                }
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    $('#editButton').click(function () {
+        console.log($('#totalInput').text());
+        console.log($('#cnt-plan').text());
+        var temp = $('#cnt-plan').text();
+        var real = $('#totalInput').text();
+        if (temp != real) {
+            $("#import-not-success .toast-title").text("เตือน");
+            $("#import-not-success .toast-body").text("กรุณาตรวจสอบข้อมูลให้ถูกต้อง");
+            $('#import-not-success').toast('show');
+            return;
+        }
+        else {
+            var data = [];
+            var isValid = true;
+            // วนลูปผ่านแต่ละแถวในตาราง
+            $("tbody tr").each(function () {
+
+                
+                var real = $(this).find(".plan-actual").data('real-val');
+                var storeId = $(this).data("store-id"); 
+                var planActual = $(this).find(".plan-actual").val(); 
+                var sku = $(this).data('val-sku');
+                var day = $(this).data('val-day');
+                var week = $(this).data('val-week');
+
+                var reasonValue = $(this).find("#reasonSelect").val();
+
+                console.log(real);
+                console.log(planActual);
+                // ตรวจสอบว่าค่าที่กรอกตรงกับค่าจริงหรือไม่
+                if (parseInt(planActual) !== real) {
+                    if (reasonValue === "0") {
+                        $("#import-not-success .toast-body").text("กรุณาเลือกเหตุผล");
+                        $('#import-not-success').toast('show');
+                        isValid = false;
+                        return; // หยุดการทำงานเมื่อพบข้อมูลที่ไม่ถูกต้อง
+                    }
+
+                }
+                // เก็บข้อมูลในรูปแบบของอ็อบเจ็กต์
+                var row = {
+                    store_id: storeId,
+                    week_no: week,
+                    day_of_week: day,
+                    reason_id: parseInt(reasonValue),
+                    sku_id: sku,
+                    plan_actually: planActual,
+                };
+                // เพิ่มอ็อบเจ็กต์ลงในอาร์เรย์
+                data.push(row);
+            });
+            console.log(data);
+            console.log(isValid);
+            //return;
+            if (isValid) {
+                var formData = new FormData();
+                formData.append('planActuals', JSON.stringify(data));
+                $.ajax({
+                    url: '/IbtOut/SubmitAll', // Update the URL based on your actual URL structure
+                    type: 'POST',
+                    data: formData,
+                    contentType: false,
+                    processData: false,
+                    success: function (data) {
+
+                        if (data.success) {
+                            $("#import-success .toast-body").text("บันทึกสำเร็จ");
+                            $('#import-success').toast('show');
+
+                        }
+                        else {
+                            $("#import-not-success .toast-body").text("บันทึกไม่สำเร็จ ข้อความ: " + data.message);
+                            $('#import-not-success').toast('show');
+                        }
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+            }
+        }
+    });
+
+
+    let mybutton = document.getElementById("btn-back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+
+    function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+});
